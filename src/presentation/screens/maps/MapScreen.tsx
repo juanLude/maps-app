b65df446218e9fcfcacb5b5b319ae40c098e3cd8
Juan Ludevid <jmludevid@hotmail.com>
2024-05-06 21:50:33 +1000

    add permissions

--- src/presentation/screens/maps/MapScreen.tsx
+++ src/presentation/screens/maps/MapScreen.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {Text, View} from 'react-native';
 
 export const MapScreen = () => {
   return (
-    <View>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>MapScreen</Text>
     </View>
   );
--- src/presentation/screens/permissions/PermissionScreen.tsx
+++ src/presentation/screens/permissions/PermissionScreen.tsx
@@ -1,10 +1,28 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Pressable, Text, View} from 'react-native';
+import {globalStyles} from '../../../config/theme/styles';
+import {usePermissionStore} from '../../store/permissions/usePermissionStore';
 
 export const PermissionScreen = () => {
+  const {locationStatus, requestLocationPermission} = usePermissionStore();
+
   return (
-    <View>
-      <Text>PermissionScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Enable location</Text>
+      <Pressable
+        style={globalStyles.btnPrimary}
+        onPress={requestLocationPermission}>
+        <Text>Enable location</Text>
+      </Pressable>
+
+      <Text>Current State: {locationStatus} </Text>
     </View>
   );
 };
