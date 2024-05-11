bc2aa05e48d621cf645878f0149bad228ac9bd23
Juan Ludevid <jmludevid@hotmail.com>
2024-05-11 12:17:20 +1000

    update moveToCurrentLocation function

--- src/presentation/components/maps/Map.tsx
+++ src/presentation/components/maps/Map.tsx
@@ -1,22 +1,38 @@
+/* eslint-disable curly */
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, {useRef} from 'react';
 
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import {Location} from '../../../infrastructure/interfaces/location';
+import {FAB} from '../ui/FAB';
+import {useLocationStore} from '../../store/location/useLocationStore';
 interface Props {
   showsUserLocation?: boolean;
   initialLocation: Location;
 }
 export const Map = ({showsUserLocation = true, initialLocation}: Props) => {
+  const mapRef = useRef<MapView>();
+  const cameraLocation = useRef<Location>(initialLocation);
+  const {getLocation} = useLocationStore();
+  const moveCameraToLocation = (location: Location) => {
+    if (!mapRef.current) return;
+    mapRef.current.animateCamera({center: location});
+  };
+  const moveToCurrentLocation = async () => {
+    const location = await getLocation();
+    if (!location) return;
+    moveCameraToLocation(location);
+  };
   return (
     <>
       <MapView
+        ref={map => (mapRef.current = map!)}
         showsUserLocation={showsUserLocation}
         provider={PROVIDER_GOOGLE} // remove if not using Google Maps
         style={{flex: 1}}
         region={{
-          latitude: initialLocation.latitude,
-          longitude: initialLocation.longitude,
+          latitude: cameraLocation.current.latitude,
+          longitude: cameraLocation.current.longitude,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}>
@@ -29,6 +45,11 @@ export const Map = ({showsUserLocation = true, initialLocation}: Props) => {
           description="description"
         />
       </MapView>
+      <FAB
+        iconName="compass-outline"
+        onPress={moveToCurrentLocation}
+        style={{bottom: 20, right: 20}}
+      />
     </>
   );
 };
--- src/presentation/components/ui/FAB.tsx
+++ src/presentation/components/ui/FAB.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {StyleProp, StyleSheet, View, ViewStyle} from 'react-native';
+import {Pressable, StyleProp, StyleSheet, View, ViewStyle} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 interface Props {
   iconName: string;
@@ -7,7 +8,13 @@ interface Props {
   style?: StyleProp<ViewStyle>;
 }
 export const FAB = ({iconName, onPress, style}: Props) => {
-  return <View style={}>FAB</View>;
+  return (
+    <View style={[styles.btn, style]}>
+      <Pressable onPress={onPress}>
+        <Icon name={iconName} size={30} color="white" />
+      </Pressable>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
